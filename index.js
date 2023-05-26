81b2b4bbeaf71111261e2a40e96e998e1b17d8e7
Samac <ianksamacs@gmail.com>
2023-05-26 13:21:57 -0300

    Escopos das requisições da API

--- index.js
+++ index.js
@@ -3,8 +3,35 @@ const api = express()
 
 
 
+//Listar por id
+api.get('/:id', (req, res) => {
+  res.status(200).send({'status':'Success'});
+});
 
+//Listar por cpf
+api.get('/cpf/:cpf', (req, res) => {
+  res.status(200).send({'status':'Success'});
+});
 
+//Listar todos
+api.get('/', (req, res) => {
+  res.status(200).send({'status':'Success'});
+});
+
+//Inserir
+api.post('/', (req, res) => {
+  res.status(200).send({'status':'Success'});
+});
+
+//Editar
+api.put('/:id', (req, res) => {
+  res.status(200).send({'status':'Success'});
+});
+
+//Deletar
+api.delete('/:id', (req, res) => {
+  res.status(200).send({'status':'Success'});
+});
 
 api.listen(3000, ()=>{
   console.log("API esta rodando")
